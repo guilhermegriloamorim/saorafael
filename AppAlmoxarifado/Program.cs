ed8156fa4819d349cbccac9d32dd58ef40ed573e
guilherme.g.amorim@gmail.com <guilherme.g.amorim@gmail.com>
2023-06-10 00:29:43 -0300

    correção de ip

--- AppAlmoxarifado/Program.cs
+++ AppAlmoxarifado/Program.cs
@@ -6,5 +6,5 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 IConfiguration Configuration = builder.Configuration;
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
-builder.Services.AddSingleton(sp => new HttpClient { BaseAddress = new Uri("http://localhost:5200/api/sr/") });
+builder.Services.AddSingleton(sp => new HttpClient { BaseAddress = new Uri("http://192.168.15.15:5200/api/sr/") });
 await builder.Build().RunAsync();
